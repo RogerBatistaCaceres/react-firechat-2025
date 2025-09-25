63ee4fcc9b0a89f0e5206437e961d2460aba53c3
batistacaceres <batistacaceres@gmail.com>
2025-09-25 22:02:55 +0200

    Add user search functionality by email in useRoomActions hook

--- src/hooks/use-room.actions.ts
+++ src/hooks/use-room.actions.ts
@@ -1,5 +1,5 @@
 import type { Room } from "@/schemas/room.schemas";
-import { collection, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useFirestore, useFirestoreCollectionData, useUser } from "reactfire";
 
 export const useRoomActions = () => {
@@ -18,6 +18,20 @@ export const useRoomActions = () => {
     suspense: true,
     idField: "id",
   });
+
+  // Buscar un user por email
+
+  const searchUserWithEmail = async (email: string) => {
+    const userRef = collection(db, "user");
+    const q = query(userRef, where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      return null;
+    }
+    const doc = querySnapshot.docs[0];
+
+    return doc.data();
+  };
 
   return { rooms: rooms as Room[] };
 };
